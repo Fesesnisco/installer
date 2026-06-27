242ec6abbd883e79c36df63e762aa3bb80ba405b
nacho <nachoturrob@proton.me>
2026-06-27 23:30:41 +0200

    Scrip de bash

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+cd ~
+
+gh auth status >/dev/null 2>&1 || gh auth login
+
+[ -d "dotfiles" ] || gh repo clone dotfiles
+
+cd ~/dotfiles
+cp .dotter/default_local.toml .dotter/local.toml
+dotter --force
+
+cd ~
+[ -d "nixos" ] || gh repo clone nixos
+
+cd ~/nixos
+sudo nixos-rebuild switch --flake .
